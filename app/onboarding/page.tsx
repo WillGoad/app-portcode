590eb998c9bb769429b41e3043e066af2ab183a9
Will Goad <willgoad@btinternet.com>
2023-09-01 18:25:55 +0100

    Fixed build issues, connected signups

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+//Convert to client component
+import { useRef, useState } from "react";
+
 import { Button } from "@/components/ui/button"
 import {
     Card,
@@ -12,37 +17,124 @@ import { Label } from "@/components/ui/label"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 
+
 export default function Onboarding() {
+    //First Tab
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [code, setCode] = useState("");
+    //Enabled bool values for each tab
+    const [emailTabDisabled, setEmailDisabled] = useState(false);
+    const [confirmationTabDisabled, setConfirmationDisabled] = useState(true);
+    const [surveyTabDisabled, setSurveyDisabled] = useState(true);
+    const [currentTab, setCurrentTab] = useState("email"); //["email", "confirmation", "survey"]
+
+    //Function for changing enabled tab
+    const setEnabledTab = (tab: string) => {
+        if (tab === "email") {
+            setEmailDisabled(false);
+            setConfirmationDisabled(true);
+            setSurveyDisabled(true);
+            setCurrentTab("email");
+        } else if (tab === "confirmation") {
+            setEmailDisabled(true);
+            setConfirmationDisabled(false);
+            setSurveyDisabled(true);
+            setCurrentTab("confirmation");
+        } else if (tab === "survey") {
+            setEmailDisabled(true);
+            setConfirmationDisabled(true);
+            setSurveyDisabled(false);
+            setCurrentTab("survey");
+        }
+    }
+
+    const onSignupVerify = async () => {
+        try {
+            const response = await fetch('https://portcode.onrender.com/api/auth/signup-verify', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ name, email }),
+            });
+            const data = response.json();
+            //If status code 200 then change tab
+            if (response.status === 200) {
+                setEnabledTab("confirmation");
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    const onSignup = async () => {
+        try {
+            const response = await fetch('https://portcode.onrender.com/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ code, email }),
+            });
+            const data = response.json();
+            //If status code 200 then change tab
+            if (response.status === 200) {
+                setEnabledTab("survey");
+            }
+            console.log(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    //Function for when the user clicks the send code button
+    const onSendCode = () => {
+        //Check email is valid
+        if (email.length > 0 && email.includes("@") && email.includes(".") && email.length > 5 && name.length > 1) {
+            console.log("Email is valid")
+            onSignupVerify();
+        }
+    }
+
+    //Function for when user clicks submit Code button
+    const onSubmitCode = () => {
+        if(code.length === 6) {
+            console.log("Code is valid");
+            onSignup();
+        }
+    }
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-                <Tabs defaultValue="account" className="w-[400px]">
+                <Tabs activationMode="automatic" value={currentTab} className="w-[400px]">
                     <TabsList className="grid w-full grid-cols-3">
-                        <TabsTrigger value="email">Email</TabsTrigger>
-                        <TabsTrigger value="confirmation">Confirm</TabsTrigger>
-                        <TabsTrigger value="survey">Survey</TabsTrigger>
+                        <TabsTrigger disabled={emailTabDisabled} value="email">Email</TabsTrigger>
+                        <TabsTrigger disabled={confirmationTabDisabled} value="confirmation">Confirm</TabsTrigger>
+                        <TabsTrigger disabled={surveyTabDisabled} value="survey">Survey</TabsTrigger>
                     </TabsList>
                     <TabsContent value="email">
                         <Card>
                             <CardHeader>
                                 <CardTitle>1 of 3</CardTitle>
                                 <CardDescription>
-                                    Add an email to use with your account. We'll send you a code to confirm it's you.
+                                    Add an email to use with your account. We&apos;ll send you a code to confirm it&apos;s you.
                                 </CardDescription>
                             </CardHeader>
                             <CardContent className="space-y-2">
                                 <div className="space-y-1">
                                     <Label htmlFor="name">Display Name</Label>
-                                    <Input id="name" defaultValue="Ryan Hoover" />
+                                    <Input id="name" value={name} onChange={e => setName(e.target.value)} />
                                 </div>
                                 <div className="space-y-1">
                                     <Label htmlFor="username">Email</Label>
-                                    <Input id="username" defaultValue="ryan@producthunt.com" />
+                                    <Input id="username" value={email} onChange={e => setEmail(e.target.value)} />
                                 </div>
                             </CardContent>
                             <CardFooter>
-                                <Button>Send Code</Button>
+                                <Button onClick={() => onSendCode()}>Send Code</Button>
                             </CardFooter>
                         </Card>
                     </TabsContent>
@@ -57,11 +149,11 @@ export default function Onboarding() {
                             <CardContent className="space-y-2">
                                 <div className="space-y-1">
                                     <Label htmlFor="name">Code</Label>
-                                    <Input id="name" />
+                                    <Input id="name" value={code} onChange={e => setCode(e.target.value)}/>
                                 </div>
                             </CardContent>
                             <CardFooter>
-                                <Button>Submit</Button>
+                                <Button onClick={() => onSubmitCode()}>Submit</Button>
                             </CardFooter>
                         </Card>
                     </TabsContent>
@@ -70,7 +162,7 @@ export default function Onboarding() {
                             <CardHeader>
                                 <CardTitle>3 of 3</CardTitle>
                                 <CardDescription>
-                                    Make changes to your account here. Click save when you're done.
+                                    Make changes to your account here. Click save when you&apos;re done.
                                 </CardDescription>
                             </CardHeader>
                             <CardContent className="space-y-2">
